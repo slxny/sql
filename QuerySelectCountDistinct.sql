8126d400f56acb0236a4eb8424a0ad107e7fc82f
Steven Levine <7097525+slxny@users.noreply.github.com>
2021-03-08 03:58:07 -0800

    Create QuerySelectCountDistinct.sql

---
+++ QuerySelectCountDistinct.sql
@@ -0,0 +1,7 @@
+
+SELECT COUNT(*) FROM (SELECT DISTINCT major_category FROM `bigquery-public-data.london_crime.crime_by_lsoa`) `bigquery-public-data.london_crime.crime_by_lsoa` 
+/*
+SELECT COUNT(DISTINCT minor_category) FROM `bigquery-public-data.london_crime.crime_by_lsoa` 
+*/
+
+
